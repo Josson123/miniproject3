c39a826e0d288d72a1abaa6f27350117ea579381
Josson123 <115704001+Josson123@users.noreply.github.com>
2025-01-05 20:38:09 +0530

    Delete mini project directory

--- mini project/user/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset(); // Unset all session variables
-session_destroy(); // Destroy the session
-
-// Redirect to home or login page after logout
-header("Location: home.php");
-exit();
-?>
